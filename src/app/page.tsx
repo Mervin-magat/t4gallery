b915c290cc5f510a820993a997c19648ec2a8564
Mervin-magat <Paulvismonte306@gmail.com>
2025-01-16 00:15:51 +0800

    picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
-import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
 
 const mockUrls = [
   "https://l83dugrv84.ufs.sh/f/JjS6TmY0oLt21NyKBiocJ4qgoxapOZy52YB0RuekHhTli6dW"
-]
+];
 
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
+
 export default function HomePage() {
   return (
-    <main className="">
-  <div className="flex flex-wrap gap 4">
-
-{mockImages.map((image) => (
-  <div key={image.id}  className="w-48"> 
-  <img src={image.url} />
-  </div>
-))
-  
-}
-    
-  </div>
+    <main className="p-4">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`Image ${image.id}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
