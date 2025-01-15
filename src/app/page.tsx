e4afd018df1bb1a772beb1620b4aa917a239b5d9
Mervin-magat <Paulvismonte306@gmail.com>
2025-01-16 00:11:45 +0800

    picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
 
+const mockUrls = [
+  "https://l83dugrv84.ufs.sh/f/JjS6TmY0oLt21NyKBiocJ4qgoxapOZy52YB0RuekHhTli6dW"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     HELLO (Gallery in Progress) whats up mga hampaslupa hehehe
+    <main className="">
+  <div className="flex flex-wrap gap 4">
+
+{mockImages.map((image) => (
+  <div key={image.id}  className="w-48"> 
+  <img src={image.url} />
+  </div>
+))
+  
+}
+    
+  </div>
     </main>
   );
 }
