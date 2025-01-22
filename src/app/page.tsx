b83b057ecd734a9128246bdfe28ee75157504ae0
Mervin-magat <Paulvismonte306@gmail.com>
2025-01-22 23:08:09 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,23 @@ import Link from "next/link";
 import { db } from "~/server/db";
 
 
-const mockUrls = [
-  
-  "https://l83dugrv84.ufs.sh/f/JjS6TmY0oLt2p0JbvSIdyANrqMHVt567FiOskouQRzea10Dl",
-  "https://l83dugrv84.ufs.sh/f/JjS6TmY0oLt2kU9O8VxiXFUI1wQij6lmTW9zMcnfs2eKHdpS",
-
-  "https://l83dugrv84.ufs.sh/f/JjS6TmY0oLt2SiLMzP37JTQO4SUzbPjFWXY50RAcqLvpZyCD"   
-
-
-  
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
 
-  const posts=await db.query.posts.findMany();
+  const image=await db.query.image.findMany({
+    orderBy: (model, {desc}) => desc (model.id),
+  });
 
   return (
     <main className="p-4">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
+        {image.map((post) => (
           <div key={post.id}>{post.name}</div>
         ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+        {image.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}  />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
