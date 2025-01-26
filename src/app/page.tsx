e219c35f1279d61cb6a81c55c04f00fbedecdb79
Mervin-magat <Paulvismonte306@gmail.com>
2025-01-26 22:27:25 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 
 export const dynamic = "force-dynamic";
 
-
-export default async function HomePage() {
-
+async function Images (){
   const image=await db.query.image.findMany({
     orderBy: (model, {asc}) => asc (model.id),
   });
+  return(
+    <div className="flex flex-wrap justify-items-center gap-4">
+    {image.map((image) => (
+      <div key={image.id} className="w-48">
+        <img src={image.url}  />
+        <div>{image.name}</div >
+      </div>
+    ))}
+  </div>
+  );
+}
 
+
+export default async function HomePage() {
   return (
     <main className="p-4">
-      <div className="flex flex-wrap gap-4">
-        {image.map((post) => (
-          <div key={post.id}></div>
-        ))}
-        {image.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url}  />
-            <div>{image.name}</div >
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full  text-2xl text-center"> Please Sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
